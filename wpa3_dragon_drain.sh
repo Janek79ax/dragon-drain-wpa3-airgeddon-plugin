411b12b2b8d168d1d6ca0e5755690cc6210af5ed
OscarAkaElvis <oscar.alfonso.diaz@gmail.com>
2025-09-01 13:56:36 +0200

    Update russian string

--- wpa3_dragon_drain.sh
+++ wpa3_dragon_drain.sh
@@ -737,7 +737,7 @@ function wpa3_dragon_drain_prehook_hookable_for_languages() {
 	arr["FRENCH","wpa3_dragon_drain_attack_18"]="Il semble que vous ne pouvez pas vous connecter à internet. Il est nécessaire parce que les composants doivent être installés pour effectuer l'attaque"
 	arr["CATALAN","wpa3_dragon_drain_attack_18"]="\${pending_of_translation} Sembla que no tens connexió a internet. És necessari ja que s’han d’instal·lar components per dur a terme l’atac"
 	arr["PORTUGUESE","wpa3_dragon_drain_attack_18"]="Parece que você não tem acesso à internet. A conexão é necessária, pois os componentes precisam ser instalados para executar o ataque"
-	arr["RUSSIAN","wpa3_dragon_drain_attack_18"]="\${pending_of_translation} Судя по всему, у вас нет Интернет доступа. Это необходимо, так как компоненты должны быть установлены для проведения атаки"
+	arr["RUSSIAN","wpa3_dragon_drain_attack_18"]="Похоже, у вас нет доступа в интернет. Это необходимо, так как для выполнения атаки нужно установить дополнительные компоненты"
 	arr["GREEK","wpa3_dragon_drain_attack_18"]="\${pending_of_translation} Φαίνεται πως δεν έχετε πρόσβαση στο διαδίκτυο. Είναι απαραίτητο δεδομένου ότι τα εξαρτήματα πρέπει να εγκατασταθούν για την εκτέλεση της επίθεσης"
 	arr["ITALIAN","wpa3_dragon_drain_attack_18"]="Sembra che non sei connesso a internet. È necessario perché i componenti devono essere installati per eseguire l'attacco"
 	arr["POLISH","wpa3_dragon_drain_attack_18"]="Nie wykryto połączenia internetowego. Jest ono potrzebne do zainstalowania wymaganych komponentów do przeprowadzenia ataku"
