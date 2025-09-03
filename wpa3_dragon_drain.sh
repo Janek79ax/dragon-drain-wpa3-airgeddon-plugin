4eb21f72f877eb232dea656dc7910d2535e4ecc5
OscarAkaElvis <oscar.alfonso.diaz@gmail.com>
2025-09-03 14:53:16 +0200

    Update arabic string

--- wpa3_dragon_drain.sh
+++ wpa3_dragon_drain.sh
@@ -743,6 +743,6 @@ function wpa3_dragon_drain_prehook_hookable_for_languages() {
 	arr["POLISH","wpa3_dragon_drain_attack_18"]="Nie wykryto połączenia internetowego. Jest ono potrzebne do zainstalowania wymaganych komponentów do przeprowadzenia ataku"
 	arr["GERMAN","wpa3_dragon_drain_attack_18"]="Es scheint, dass Sie keine Internetverbindung haben. Eine Verbindung ist notwendig, da Komponenten installiert werden müssen, um den Angriff auszuführen"
 	arr["TURKISH","wpa3_dragon_drain_attack_18"]="İnternet erişiminiz yok gibi görünüyor. Saldırının gerçekleştirilebilmesi için bileşenlerin kurulması gerektiğinden internet erişimi gereklidir"
-	arr["ARABIC","wpa3_dragon_drain_attack_18"]="يبدو أنه لا يوجد اتصال بالإنترنت. هذا ضروري لأن يجب تثبيت بعض المكوّنات لتنفيذ الهجوم"
+	arr["ARABIC","wpa3_dragon_drain_attack_18"]="يبدو أنه ليس لديك اتصال بالإنترنت. يعد الاتصال ضروريًا لأن المكوّنات يجب أن تُثبَّت لتنفيذ الهجوم"
 	arr["CHINESE","wpa3_dragon_drain_attack_18"]="您似乎无法访问互联网。但必须安装必要组件才能进行下一步攻击"
 }
